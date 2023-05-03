1c2ee8f70d06c917580bda27a195cb8154fd4df0
ShrutiKhanal <shruti.khanal2002@gmail.com>
2023-05-03 09:30:28 +0545

    addition

--- day_7/git.py
+++ day_7/git.py
@@ -1,2 +1,26 @@
 # git init
-#git add .
+# git add . (git add --all) #boundry bhitra lerauna
+# git commit -m "<commit_message>" #(-m is message)
+#git remote add origin <ssh link>
+# git push -u  origin <branch_name>
+
+#git status #Shows the current status of all git staged and unstaged files.
+
+##Setting git configuration
+#1. git config --global user.name "user.name"
+#2. git config --global user.email "user.email"
+
+#generating ssh key pair
+#ssh-keygen -t rsa -b 4096 -C "your_email@example.com"
+
+
+#Git branching
+#git branch
+#It shows all the branches and highlightd the current branch
+
+#git branch <branch_name> #Creates a new branch
+#git checkout <branch_name> #jumps/checkout to the next branch
+
+#git checkout -b <branch_name> #creating a new branch and jumping to that branch
+
+
